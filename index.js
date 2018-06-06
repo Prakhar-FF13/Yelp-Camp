5a6ac21b618af6e04d7d4fb2bbaf72c0559fafd2
Prakhar-FF13 <prakhar.meerut@gmail.com>
2018-06-06 23:04:44 +0530

    Adding the comments form and button to add comments to a campgrounds. Comment routes added too.

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ app 		    = express(),
 bodyParser  = require("body-parser"),
 mongoose    = require("mongoose"),
 Campground  = require("./models/campground"),
-seedDB      = require("./seeds");
+seedDB      = require("./seeds"),
+Comment     = require("./models/comment");
 
 seedDB();
 
@@ -64,6 +65,38 @@ app.get("/campgrounds/:id", function(req, res){
 		}
 		else{
 			res.render("show", {campground: foundCampground});
+		}
+	});
+});
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+	Campground.findById(req.params.id, function(err, campground){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.render("comments/new", {campground: campground});
+		}
+	});
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+	Campground.findById(req.params.id, function(err, campground){
+		if(err){
+			console.log(err);
+			res.redirect("/campgrounds");
+		}
+		else{
+			Comment.create(req.body.comment, function(err, comment){
+				if(err){
+					console.log(err);
+				}
+				else{
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect("/campgrounds/" + campground._id);
+				}
+			});
 		}
 	});
 });
