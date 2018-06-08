8ec17672e9dd92fa895f0073c4322e454c44d34d
Prakhar-FF13 <prakhar.meerut@gmail.com>
2018-06-08 21:12:18 +0530

    Adding Login and Logout Routes and making form for login.

--- index.js
+++ index.js
@@ -125,6 +125,22 @@ app.post("/register", function(req, res){
 
 });
 
+app.get("/login", function(req, res){
+		res.render("login");
+});
+
+app.post("/login", passport.authenticate("local",
+ 		{
+	 		successRedirect:"/campgrounds",
+			failureRedirect:"/login"
+		}), function(req, res){}
+);
+
+app.get("/logout", function(req, res){
+		req.logout();
+		res.redirect("/campgrounds");
+});
+
 app.listen(3000, function(){
 	console.log("Yelp Camp Server has started");
 });
