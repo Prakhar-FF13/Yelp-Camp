5d69bdfe1d6d21e945c2108fac236f9382122bf3
Prakhar-FF13 <prakhar.meerut@gmail.com>
2018-06-09 20:07:04 +0530

    Only registered person can add a new campground.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -14,11 +14,15 @@ router.get("/", function(req, res){
 });
 
 // Add a new Campground
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
 	var name = req.body.name;
 	var image = req.body.image;
 	var desc = req.body.description;
-	var newCampground = {name: name, image: image, description: desc};
+	var author = {
+			id: req.user._id,
+			username: req.user.username
+	};
+	var newCampground = {name: name, image: image, description: desc, author: author};
 	Campground.create(newCampground, function(err, newCampground){
 		if(err){
 			console.log(err);
@@ -30,7 +34,7 @@ router.post("/", function(req, res){
 });
 
 // Display new campground form
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
 	res.render("new");
 });
 
@@ -45,5 +49,14 @@ router.get("/:id", function(req, res){
 		}
 	});
 });
+
+function isLoggedIn(req, res, next){
+		if(req.isAuthenticated()){
+				return next();
+		}
+		else{
+				res.redirect("/login");
+		}
+}
 
 module.exports = router;
